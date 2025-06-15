8c2ec6c0430e2e5b838a175a3c4448b86112ca99
robokae <alexanderhom19@gmail.com>
2025-06-15 14:58:36 -0700

    Add gap when there is only one window

--- dwm.c
+++ dwm.c
@@ -1663,8 +1663,8 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	if (c->isfloating || c->mon->lt[c->mon->sellt]->arrange == NULL) {
 		gapincr = gapoffset = 0;
 	} else {
-		/* Remove border and gap if layout is monocle or only one client */
-		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
+		/* Remove border and gap if layout is monocle */
+		if (c->mon->lt[c->mon->sellt]->arrange == monocle) {
 			gapoffset = 0;
 			gapincr = -2 * borderpx;
 			wc.border_width = 0;
